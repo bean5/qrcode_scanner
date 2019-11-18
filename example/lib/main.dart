94eae220a44f57913343d7709c94a8fc1601d15b
bean5 <2052646+bean5@users.noreply.github.com>
2019-11-18 16:43:23 -0700

    Display codes and buttons

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,9 +4,8 @@ import 'dart:typed_data';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:qrscan/qrscan.dart' as scanner;
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,166 +17,82 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List _scanned_codes = List<String>();
   Uint8List bytes = Uint8List(0);
-  TextEditingController _inputController;
   TextEditingController _outputController;
+
+  get _scanned_code_widgets => _scanned_codes
+      .map<Widget>((code) => Card(
+              child: Row(
+            children: <Widget>[
+              Text(
+                "code",
+                style: TextStyle(color: Colors.black),
+              ),
+              FlatButton(
+                child: Icon(Icons.content_copy),
+                onPressed: null,
+              ),
+              FlatButton(
+                child: Icon(Icons.save),
+                onPressed: null,
+              ),
+              FlatButton(
+                child: Icon(Icons.delete),
+                onPressed: () {
+                  setState(() {
+                    _scanned_codes.remove(code);
+                  });
+                },
+              )
+            ],
+          )))
+      .toList();
+
+  void _reset() {
+    setState(() {
+      _scanned_codes.clear();
+    });
+  }
 
   @override
   initState() {
     super.initState();
-    this._inputController = TextEditingController();
     this._outputController = TextEditingController();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
       home: Scaffold(
-        backgroundColor: Colors.grey[300],
         body: Builder(
           builder: (BuildContext context) {
-            return ListView(
-              children: <Widget>[
-                _qrCodeWidget(this.bytes, context),
-                Container(
-                  color: Colors.white,
-                  child: Column(
+            return Center(
+              child: Column(
+                children: <Widget>[
+                  Column(
                     children: <Widget>[
-                      TextField(
-                        controller: this._inputController,
-                        keyboardType: TextInputType.url,
-                        textInputAction: TextInputAction.go,
-                        onSubmitted: (value) => _generateBarCode(value),
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.text_fields),
-                          helperText: 'Please input your code to generage qrcode image.',
-                          hintText: 'Please Input Your Code',
-                          hintStyle: TextStyle(fontSize: 15),
-                          contentPadding: EdgeInsets.symmetric(horizontal: 7, vertical: 15),
-                        ),
+                      SizedBox(height: 150),
+                      Container(
+                        height: 390,
+                        child: _scanned_codes.isEmpty
+                            ? Text(
+                                'The codes you scan will be displayed in this area.')
+                            : Column(
+                                children: _scanned_code_widgets,
+                              ),
                       ),
-                      SizedBox(height: 20),
-                      TextField(
-                        controller: this._outputController,
-                        maxLines: 2,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.wrap_text),
-                          helperText: 'The barcode or qrcode you scan will be displayed in this area.',
-                          hintText: 'The barcode or qrcode you scan will be displayed in this area.',
-                          hintStyle: TextStyle(fontSize: 15),
-                          contentPadding: EdgeInsets.symmetric(horizontal: 7, vertical: 15),
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      this._buttonGroup(),
-                      SizedBox(height: 70),
                     ],
                   ),
-                ),
-              ],
+                ],
+              ),
             );
           },
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => _scanBytes(),
-          tooltip: 'Take a Photo',
-          child: const Icon(Icons.camera_alt),
-        ),
-      ),
-    );
-  }
-
-  Widget _qrCodeWidget(Uint8List bytes, BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(20),
-      child: Card(
-        elevation: 6,
-        child: Column(
-          children: <Widget>[
-            Container(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  Icon(Icons.verified_user, size: 18, color: Colors.green),
-                  Text('  Generate Qrcode', style: TextStyle(fontSize: 15)),
-                  Spacer(),
-                  Icon(Icons.more_vert, size: 18, color: Colors.black54),
-                ],
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 9),
-              decoration: BoxDecoration(
-                color: Colors.black12,
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(4), topRight: Radius.circular(4)),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 40, right: 40, top: 30, bottom: 10),
-              child: Column(
-                children: <Widget>[
-                  SizedBox(
-                    height: 190,
-                    child: bytes.isEmpty
-                        ? Center(
-                            child: Text('Empty code ... ', style: TextStyle(color: Colors.black38)),
-                          )
-                        : Image.memory(bytes),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 7, left: 25, right: 25),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        Expanded(
-                          flex: 5,
-                          child: GestureDetector(
-                            child: Text(
-                              'remove',
-                              style: TextStyle(fontSize: 15, color: Colors.blue),
-                              textAlign: TextAlign.left,
-                            ),
-                            onTap: () => this.setState(() => this.bytes = Uint8List(0)),
-                          ),
-                        ),
-                        Text('|', style: TextStyle(fontSize: 15, color: Colors.black26)),
-                        Expanded(
-                          flex: 5,
-                          child: GestureDetector(
-                            onTap: () async {
-                              final success = await ImageGallerySaver.saveImage(this.bytes);
-                              SnackBar snackBar;
-                              if (success) {
-                                snackBar = SnackBar(content: Text('Successful Preservation!'));
-                                Scaffold.of(context).showSnackBar(snackBar);
-                              } else {
-                                snackBar = SnackBar(content: Text('Save failed!'));
-                              }
-                            },
-                            child: Text(
-                              'save',
-                              style: TextStyle(fontSize: 15, color: Colors.blue),
-                              textAlign: TextAlign.right,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Divider(height: 2, color: Colors.black26),
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Icon(Icons.history, size: 16, color: Colors.black38),
-                  Text('  Generate History', style: TextStyle(fontSize: 14, color: Colors.black38)),
-                  Spacer(),
-                  Icon(Icons.chevron_right, size: 16, color: Colors.black38),
-                ],
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 9),
-            )
-          ],
+        bottomNavigationBar: BottomAppBar(
+          child: this._buttonGroup(),
         ),
       ),
     );
@@ -186,27 +101,6 @@ class _MyAppState extends State<MyApp> {
   Widget _buttonGroup() {
     return Row(
       children: <Widget>[
-        Expanded(
-          flex: 1,
-          child: SizedBox(
-            height: 120,
-            child: InkWell(
-              onTap: () => _generateBarCode(this._inputController.text),
-              child: Card(
-                child: Column(
-                  children: <Widget>[
-                    Expanded(
-                      flex: 2,
-                      child: Image.asset('images/generate_qrcode.png'),
-                    ),
-                    Divider(height: 20),
-                    Expanded(flex: 1, child: Text("Generate")),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
         Expanded(
           flex: 1,
           child: SizedBox(
@@ -249,17 +143,86 @@ class _MyAppState extends State<MyApp> {
             ),
           ),
         ),
+        Expanded(
+          flex: 1,
+          child: SizedBox(
+            height: 120,
+            child: FlatButton(
+              onPressed: _scanned_codes.isEmpty ? null : _saveCodes,
+              child: Card(
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 2,
+                      child: Icon(Icons.save),
+                    ),
+                    Divider(height: 20),
+                    Expanded(
+                        flex: 1,
+                        child: Text(_scanned_codes.length == 1
+                            ? "Save Code"
+                            : "Save Codes")),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: SizedBox(
+            height: 120,
+            child: FlatButton(
+              onPressed: _scanned_codes.isEmpty ? null : _reset,
+              child: Card(
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 2,
+                      child: Icon(Icons.clear),
+                    ),
+                    Divider(height: 20),
+                    Expanded(
+                      flex: 1,
+                      child: Text("Reset"),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
 
   Future _scan() async {
-    String barcode = await scanner.scan();
-    if (barcode == null) {
+    // final String code = await scanner.scan();
+    final String code = "test";
+    if (code == null) {
       print('nothing return.');
     } else {
-      this._outputController.text = barcode;
+      // this._outputController.text = code;
+      setState(() {
+        _scanned_codes.add(code);
+      });
     }
+  }
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    final date = DateTime.now();
+    return File('$path/animal tags $date.csv');
+  }
+
+  Future<File> _saveCodes() async {
+    return (await _localFile)
+        .writeAsString("tag\n" + _scanned_codes.join("\n"));
   }
 
   Future _scanPhoto() async {
@@ -270,17 +233,5 @@ class _MyAppState extends State<MyApp> {
   Future _scanPath(String path) async {
     String barcode = await scanner.scanPath(path);
     this._outputController.text = barcode;
-  }
-
-  Future _scanBytes() async {
-    File file = await ImagePicker.pickImage(source: ImageSource.camera);
-    Uint8List bytes = file.readAsBytesSync();
-    String barcode = await scanner.scanBytes(bytes);
-    this._outputController.text = barcode;
-  }
-
-  Future _generateBarCode(String inputCode) async {
-    Uint8List result = await scanner.generateBarCode(inputCode);
-    this.setState(() => this.bytes = result);
   }
 }
